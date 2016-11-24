c0f5206faea4c6d743f7be9fa931858410413853
Kai Curry <kai@cape.io>
2016-11-23 18:32:31 -0600

    v3.0.2 collection@4

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import * as reducerIndex from './reducer'
+import { selectYear } from './session'
 
 export {
   reducerIndex,
+  selectYear,
 }
 const reducer = combineReducers(reducerIndex)
 
