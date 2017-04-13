e474e1d0d7a7793fe46dcb71a03a7b0672075d1b
Kai Curry <kai@cape.io>
2017-04-13 17:54:22 -0400

    v3.8.1 config

--- src/db/select.js
+++ src/db/select.js
@@ -1,5 +1,8 @@
 import { partial, property } from 'lodash'
 import { select } from 'cape-select'
 
+export const selectConfig = property('config')
+export const selectConfigPath = partial(select, selectConfig)
+
 export const selectDb = property('db')
 export const selectDbPath = partial(select, selectDb)
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import * as reducerIndex from './reducer'
 import { selectYear } from './session'
 
 export * from './db/actions'
+export * from './db/select'
+
 export {
   reducerIndex,
   selectYear,
